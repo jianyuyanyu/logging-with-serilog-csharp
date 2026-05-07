21ce55189f55d4b8524629927c49cfacf4cb1264
Karen Payne <paynekaren@comcast.net>
2026-05-07 09:34:44 -0700

    Add SerilogReadSettingsFromJson sample project
    by Karen

---
+++ SerilogReadSettingsFromJson/Classes/Configuration/SetupLogging.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using static System.DateTime;
+
+namespace SerilogReadSettingsFromJson.Classes.Configuration;
+internal class SetupLogging
+{
+    /// <summary>
+    /// Configures Serilog logging for development environments with an alternate setup.
+    /// </summary>
+    /// <remarks>
+    /// This method initializes a Serilog logger that writes log entries to a file. 
+    /// The log file is stored in a directory named after the current date (in the format "YYYY-MM-DD") 
+    /// within the "LogFiles" folder in the application's base directory.
+    /// </remarks>
+    public static void DevelopmentAlternate()
+    {
+
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LogFiles", $"{Now.Year}-{Now.Month:D2}-{Now.Day:D2}", "Log.txt"),
+                rollingInterval: RollingInterval.Infinite,
+                outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] {Message}{NewLine}{Exception}")
+            .CreateLogger();
+    }
+    
+    /// <summary>
+    /// Configures Serilog logging for development environments using settings from a JSON configuration file.
+    /// </summary>
+    /// <remarks>
+    /// This method reads logging configuration from the `appsettings.json` file located in the application's base directory.
+    /// It creates and initializes a Serilog logger based on the settings defined in the JSON file.
+    /// </remarks>
+    public static void Development()
+    {
+        
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration) // requires Serilog.Settings.Configuration package
+            .CreateLogger();
+    }
+}
---
+++ SerilogReadSettingsFromJson/Program.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+using SerilogReadSettingsFromJson.Classes.Core;
+using Spectre.Console;
+
+namespace SerilogReadSettingsFromJson;
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+
+        ThrowException();
+
+        SpectreConsoleHelpers.ExitPrompt(Justify.Left);
+    }
+
+
+    private static void ThrowException()
+    {
+        try
+        {
+            var lines = File.ReadAllLines("NonExistentFile.txt");
+            SpectreConsoleHelpers.PinkPill(Justify.Left, "read file");
+        }
+        catch (Exception e)
+        {
+           Log.Error(e, "An error occurred while reading the file."); 
+           SpectreConsoleHelpers.ErrorPill(Justify.Left, "An error occurred while reading the file.");
+           
+        }
+    }
+
+}
